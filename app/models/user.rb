1fea469f39480032f695c1da7a51289eeec18d21
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-08-20 00:21:14 +0900

    いいね機能&画像横

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :reviews
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :tweet
 end
