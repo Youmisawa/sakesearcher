34181b52417c4c065e669c9fe49a698705515527
Youmisawa <66550111+Youmisawa@users.noreply.github.com>
2020-08-19 16:14:34 +0900

    search Ajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_023143) do
+ActiveRecord::Schema.define(version: 2020_08_18_140312) do
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "tweet_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_023143) do
     t.bigint "user_id", null: false
     t.integer "alc"
     t.text "url"
+    t.index ["tweetname"], name: "index_tweets_on_tweetname", length: 32
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
